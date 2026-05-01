710b375150f1fb7c949330beeff14114f2efcc3c
Manav Singla <manavsinghla123@gmail.com>
2026-05-01 14:43:49 +0530

    Add TestMail class and email view for sending test emails

---
+++ MyProject3/resources/views/emails/test.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test Email</title>
+</head>
+<body>
+    <h1>This is a test email sent from your Laravel application!</h1>
+    <p>Message: {{ $messageText }}</p>
+    <p>Thank you.</p>
+</body>
+</html>
--- MyProject3/routes/web.php
+++ MyProject3/routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\TestMail;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/send-mail', function () {
+    $messageText = "Hello! If you're seeing this, your Laravel mail configuration is working.";
+    Mail::to('ashutoshydv.connect@gmail.com')->send(new TestMail($messageText));
+    
+    return "Mail dispatched! Check your storage/logs/laravel.log or email inbox.";
 });
 
 Route::get('/abc',[UploadYZController::class,'show']);
